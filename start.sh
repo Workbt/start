bdf32eff2b2db9ae936bf74f999165859785d4b4
Workbt <87547432+Workbt@users.noreply.github.com>
2021-07-17 15:33:41 +0300

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,16 @@
+apt update
+apt upgrade
+apt install python3.9 -y
+apt install python3-pip-y
+apt install screen -y
+pip3 install aiogram
+pip3 install rich
+pip3 install pyyaml
+apt install python3-venv
+python3 -m venv work
+source work/bin/activate
+apt update
+pip install aiogram
+pip insstall rich
+pip install pyyaml
+screen -S xxx python -m app
